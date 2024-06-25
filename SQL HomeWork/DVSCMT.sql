1cf6ae972a618041fbf8cab932808a2899897c56
DucThien <nguyenducthienlq1@gmail.com>
2024-06-25 20:09:59 +0700

    Them SQL homework

---
+++ SQL HomeWork/DVSCMT.sql
@@ -0,0 +1,141 @@
+﻿Create database DVSCMT
+use DVSCMT
+Create table KHACHHANG (
+	MaKH varchar(5) not null,
+	TenKH nvarchar (20),
+	NgaySinh date,
+	DiaChi nvarchar (10),
+	primary key (MaKH)
+)
+Create table DICHVU (
+	MaDV varchar (10) not null,
+	TenDV nvarchar (20),
+	DVT nvarchar (5),
+	DonGia int,
+	primary key (MaDV)
+)
+Create table PHATSINH (
+	SoPhieu varchar (5) not null,
+	NgayLap date,
+	MaKH varchar (5) not null,
+	primary key (SoPhieu),
+	foreign key (MaKH) references KHACHHANG(MaKH)
+	)
+Create table PHATSINHCHITIET (
+	SoPhieu varchar (5) not null,
+	MaDV varchar (10) not null,
+	SoLuong int,
+	primary key (SoPhieu, MaDV),
+	foreign key (SoPhieu) references PHATSINH(SoPhieu),
+	foreign key (MaDV) references DICHVU(MaDV)
+)
+Create table PHIEUTHU (
+	SoPT varchar (5) not null,
+	MaKH varchar (5) not null,
+	Ngay date,
+	SoTien int,
+	primary key (SoPT),
+	foreign key (MaKH) references KHACHHANG(MaKH)
+)
+Set Dateformat DMY
+Insert into KHACHHANG (MaKH, TenKH, NgaySinh, Diachi) 
+			values ('HNV', N'Nguyễn Văn Hoàng', '01/05/1992', N'TP.HCM'),
+				   ('HTM', N'Trần Mộng Hoàng', '12/03/1989', N'Lâm Đồng'),
+				   ('HVH', N'Vũ Hoàng Hải', '02/05/1982', N'Long An'),
+				   ('TNT', N'Nguyễn Thị Thanh', '12/03/1989', N'Trà Vinh'),
+				   ('TVTA', N'Vũ Thị Ánh Tuyết', '03/03/1987', N'Cà Mau')
+Insert into DICHVU (MaDV, TenDV, DVT, DonGia) 
+			values ('BanPhim', N'Sửa bàn phím', N'Cái', 20000),
+				   ('Chuot', N'Sửa chuột', N'Con', 15000),
+				   ('CPU', N'Sửa CPU', N'Cái', 200000),
+				   ('Main', N'Sửa main', N'Cái', 180000),
+				   ('Nguon', N'Sửa nguồn', N'Cái', 150000),
+				   ('Win', N'Cài lại win', N'Lần', 200000)
+Insert into PHATSINH (SoPhieu, NgayLap, MaKH)
+			values ('SC01', '01/02/2018', 'HNV'),
+				   ('SC02', '01/03/2018', 'HVH'),
+				   ('SC03', '05/03/2018', 'TNT'),
+				   ('SC04', '06/04/2018', 'TVTA'),
+				   ('SC05', '06/04/2018', 'HNV'),
+				   ('SC06', '20/05/2018', 'TNT'),
+				   ('SC07', '06/08/2018', 'HVH')
+Insert into PHATSINHCHITIET (SoPhieu, MaDV, SoLuong)
+			values ('SC01', 'CPU', 1),
+			       ('SC01', 'Win', 5),
+				   ('SC02', 'CPU', 1),
+				   ('SC03', 'Main', 1),
+				   ('SC04', 'BanPhim', 126),
+				   ('SC04', 'Chuot', 32),
+				   ('SC04', 'CPU', 1),
+				   ('SC05', 'BanPhim', 125),
+				   ('SC05', 'CPU', 1),
+				   ('SC05', 'Nguon', 86),
+				   ('SC06', 'BanPhim', 149),
+				   ('SC06', 'Chuot', 48),
+				   ('SC06', 'Main', 1),
+				   ('SC07', 'BanPhim', 94),
+				   ('SC07', 'Chuot', 17),
+				   ('SC07', 'CPU', 1),
+				   ('SC07', 'Win', 1)
+Insert into PHIEUTHU (SoPT, MaKH, Ngay, SoTien)
+			values ('P001', 'HNV', '03/03/2018', 2975000),
+				   ('P002', 'TNT', '11/05/2018', 2485000),
+				   ('P003', 'HNV', '01/06/2018', 2500000),
+				   ('P004', 'TVTA', '07/08/2018', 3110000),
+				   ('P005', 'TNT', '23/06/2018', 3265000),
+				   ('P006', 'HNV', '02/10/2018', 2235000)
+select * from KHACHHANG
+select * from DICHVU
+select * from PHATSINH
+select * from PHATSINHCHITIET
+select * from PHIEUTHU
+--a Tạo query liệt kê khách hàng phát sinh tổng phí dịch vụ sửa chữa nhỏ nhất
+Select top 1 K.MaKH, K.TenKH, K.NgaySinh, K.DiaChi, sum (PH.SoLuong * D.DonGia) as N'Tổng Phí DV'
+from KHACHHANG K join PHATSINH P on K.MaKH = P.MaKH
+				 join PHATSINHCHITIET PH on P.SoPhieu = PH.SoPhieu
+				 join DICHVU D on PH.MaDV = D.MaDV
+group by K.MaKH, K.TenKH, K.NgaySinh, K.DiaChi
+order by sum (PH.SoLuong * D.DonGia) asc 
+
+--b Tạo query tìm các khách hàng có phát sinh phí dịch vụ sửa chữa nhưng chưa thanh toán
+select MaKH, TenKH, NgaySinh, DiaChi
+from KHACHHANG 
+where MaKH in(
+select MaKH
+from PHATSINH 
+where MaKH not in ( 
+	select MaKH 
+	from PHIEUTHU 
+			)
+)
+--c Tạo query tìm các khách hàng sửa chữa cả 2 dịch vụ 'Chuot', 'CPU'
+select *
+from KHACHHANG
+where MaKH in (
+select MaKH
+from PHATSINH 
+where SoPhieu in(
+select SoPhieu
+from PHATSINHCHITIET
+where MaDV like 'Chuot' and SoPhieu in (
+select SoPhieu
+from PHATSINHCHITIET 
+where MaDV like 'CPU'
+  )
+ )
+)
+--d tạo query hiển thị thông tin của các phiếu sử dụng nhiều dịch vụ nhất
+select *
+from PHATSINH
+where SoPhieu = (
+	select top 1 PH.SoPhieu 
+	from PHATSINHCHITIET PH join PHATSINH P on PH.SoPhieu = P.SoPhieu
+	group by PH.SoPhieu
+	order by sum(PH.SoLuong) desc 
+	)
+--e Tạo query cho biết thông tin đầy đủ của khách hàng chưa từng có sửa chữa gì
+select *
+from KHACHHANG 
+where MaKH not in (
+	select MaKH
+	from PHATSINH )
---
+++ SQL HomeWork/QUANLILUONG.sql
@@ -0,0 +1,92 @@
+﻿create database QUANLILUONG
+use QUANLILUONG
+create table PHONGBAN(
+	MaPB varchar(5) not null,
+	TenPB nvarchar(50),
+	primary key(MaPB),
+)
+create table NHANVIEN(
+	MaNV varchar(5) not null,
+	HoTen nvarchar(50),
+	NgaySinh date,
+	MaPB varchar(5) not null,
+	MucLuong float,
+	primary key(MaNV),
+	foreign key(MaPB) references PHONGBAN(MaPB)
+)
+create table TAMUNG(
+	SoPhieu varchar(5) not null,
+	Ngay date,
+	MaNV varchar(5) not null,
+	SoTien float,
+	primary key(SoPhieu),
+	foreign key (MaNV) references NHANVIEN(MaNV)
+)
+create table CHAMCONG(
+	MaNV varchar(5) not null,
+	NgayGhiSo date not null,
+	SoNgayCong int,
+	primary key(MaNV,NgayGhiSo),
+	foreign key(MaNV) references NHANVIEN(MaNV)
+)
+insert into PHONGBAN(MaPB,TenPB)
+values	('P01',N'Hành chính'),
+		('P02',N'Kinh Doanh'),
+		('P03',N'Kĩ thuật'),
+		('P04',N'Kế toán')
+set dateformat dmy 
+select * from PHONGBAN
+insert into NHANVIEN (MaNV,HoTen,NgaySinh,MaPB,MucLuong)
+values	('NV1',N'Nguyễn Lê Quang','04/05/1989','P02',8000000),
+		('NV2',N'Trần Thu Hồng','01/05/1990','P04',7500000),
+		('NV3',N'Hoàng Vũ Trần','04/05/1989','P03',9000000),
+		('NV4',N'Ngô Việt Hùng','03/05/1990','P01',8000000),
+		('NV5',N'Vũ Thanh Giang','01/05/1987','P02',5000000),
+		('NV6',N'Trần Cao Cường','23/05/1986','P03',6000000),
+		('NV7',N'Cao Thanh Sang','21/06/1985','P02',5000000)
+select * from NHANVIEN	
+insert into CHAMCONG(MaNV,NgayGhiSo,SoNgayCong)
+values	('NV1','31/01/2019',24),
+		('NV1','28/02/2019',25),
+		('NV2','31/01/2019',20),
+		('NV2','28/02/2019',22),
+		('NV3','31/01/2019',26),
+		('NV3','28/02/2019',5),
+		('NV4','31/01/2019',20),
+		('NV4','28/02/2019',25),
+		('NV5','28/02/2019',18),
+		('NV6','31/01/2019',24),
+		('NV7','28/02/2019',4)
+insert into TAMUNG(SoPhieu,Ngay,MaNV,SoTien)
+values	('P01','05/01/2018','NV3',2000000),
+		('P02','08/01/2018','NV4',1500000),
+		('P03','10/02/2018','NV1',1200000),
+		('P04','12/02/2018','NV3',1000000)
+--a. 
+select n1.MaNV,n1.HoTen,n1.NgaySinh,n1.MaPB
+from NHANVIEN n1, NHANVIEN n2
+where n1.NgaySinh = n2.NgaySinh and n1.MaNV <> n2.MaNV
+--b
+select top 1 p.MaPB,p.TenPB,count(n.MaNV) as N'S? nhân viên'
+from PHONGBAN p join NHANVIEN n on p.MaPB = n.MaPB
+group by p.MaPB,p.TenPB
+order by count(n.MaNV) desc
+--c
+select n.MaNV,n.HoTen,n.NgaySinh,n.MaPB
+from NHANVIEN n
+where n.MaNV not in(
+					select c.MaNV
+					from CHAMCONG c		
+					where c.NgayGhiSo like '%2019-01%'	
+)
+--d
+select *
+from NHANVIEN
+where MaNV not in (
+				select MaNV
+				from TAMUNG
+)
+--e
+select p.MaPB,p.TenPB,count(n.MaNV) as N'S? nhân viên'
+from PHONGBAN p join NHANVIEN n on p.MaPB = n.MaPB
+group by p.MaPB,p.TenPB
